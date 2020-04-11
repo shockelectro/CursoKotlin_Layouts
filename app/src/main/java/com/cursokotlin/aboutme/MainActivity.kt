384ed906fb9b1d7769372625613a87687de25892
shockelectro <43023461+shockelectro@users.noreply.github.com>
2020-04-11 16:53:58 -0300

    Styling a TextView

--- app/src/main/java/com/cursokotlin/aboutme/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/aboutme/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Conecta con el layout xml
+        //Conecta con el layout xmln
         setContentView(R.layout.activity_main)
     }
 }
